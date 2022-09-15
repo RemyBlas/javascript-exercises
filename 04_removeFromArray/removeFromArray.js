12eed0d42384b01365072f8dad86ac1ee2024ed0
Leandro Blasco <lsblasco@hotmail.com>
2022-09-15 20:45:06 -0300

    complete test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array = [], ...args) {
+    const items = Array.from(args)
+
+    for (let i=0; i<items.length; i++) {
+        if (array.includes(items[i])) {
+            let x = array.indexOf(items[i])
+            array.splice(x,1)
+        }
+    }
+    return array
 
 };
 

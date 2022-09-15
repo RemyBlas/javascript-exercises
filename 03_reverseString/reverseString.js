a4c98957f4bfc04ba63553bf22f954922b2cae6b
Leandro Blasco <lsblasco@hotmail.com>
2022-09-15 19:25:28 -0300

    complete test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const splitted = string.split('')
+    let reversed = ''
 
+    for (let i=splitted.length-1; i>=0; i--) {
+        reversed += splitted[i]
+    }
+
+    return reversed
 };
 
 // Do not edit below this line

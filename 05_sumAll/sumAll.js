d63cde3d41a0875ce5d66e4f774a28dbc066666b
Leandro Blasco <lsblasco@hotmail.com>
2022-09-15 22:34:07 -0300

    complete test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    const arrangedArray = args.sort();
+    const a = arrangedArray[0];
+    const b = arrangedArray[1];
+    let sum = 0;
 
-};
+    if (typeof a == 'number' && typeof b == 'number') {
+        for (let i=a; i<=b; i++) {
+            sum += i;
+        }
+    } else return 'ERROR'
+    
+    if (sum > 0) {
+        return sum
+    } else return 'ERROR'
+}
 
 // Do not edit below this line
 module.exports = sumAll;

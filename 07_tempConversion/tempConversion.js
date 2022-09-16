9ad54c966af089d8121064bbc755c18780393713
Leandro Blasco <lsblasco@hotmail.com>
2022-09-16 00:14:55 -0300

    complete test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  const c = (f-32) * 5/9;
+  const rounded = Math.round(c*10)/10;
+  return rounded;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  const f = c * 9/5 + 32;
+  const rounded = Math.round(f*10)/10;
+  return rounded;
 };
 
 // Do not edit below this line
